41fad53b622ecaa39c97e32587fb3a85a2c1d73d
MadathalaMounika <138894489+MadathalaMounika@users.noreply.github.com>
2024-07-04 14:00:44 +0530

    Update build_files.sh

--- website1/build_files.sh
+++ website1/build_files.sh
@@ -1,7 +1,6 @@
-#!/bin/bash
-
-pip install -r requirements.txt
-python manage.py collectstatic --noinput
-python manage.py migrate
+echo "BUILD HASH"
+python3.9 -m pip install -r requirements.txt
+python3.9 manage.py collectstatic --noinput --clear
+echo "BUILD END"
 
 
